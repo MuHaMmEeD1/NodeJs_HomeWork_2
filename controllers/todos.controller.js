2a3684d9c0c90c1253e87c7289e7defa954f7687
Muhamməd Nəbiyev <aliyevasamire596@gmail.com>
2025-04-28 16:47:10 +0400

    c2

--- controllers/todos.controller.js
+++ controllers/todos.controller.js
@@ -1,61 +1,84 @@
 import { Todo } from "../models/todo.model.js";
+import { getTokenContents } from "../utils/getTokenContents.js";
 
 export const getAllTodos = async (req, res) => {
-    try {
-        const todos = await Todo.find();
-        res.status(200).json(todos);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const todos = await Todo.find().populate("author", "name surname email");
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const getTodoById = async (req, res) => {
-    try {
-        const todo = await Todo.findById(req.params.id);
-        if (!todo) {
-            return res.status(404).json({ message: "Todo not found" });
-        }
-        res.status(200).json(todo);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
     }
-}
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getMyAllTodos = async (req, res) => {
+  try {
+    const { accessToken } = req.cookies;
+    const { id, email } = getTokenContents(res, accessToken);
+
+    if (!user) return;
+
+    const todos = await Todo.find({ authorId: id }).populate(
+      "author",
+      "name surname email"
+    );
+    res.status(200).json({ success: true, todos });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 export const createTodo = async (req, res) => {
-    const { title, description } = req.body;
-    const todo = new Todo({
-        title,
-        description
-    });
-    try {
-        const savedTodo = await todo.save();
-        res.status(201).json(savedTodo);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+  const { accessToken } = req.cookies;
+  const { id, email } = getTokenContents(res, accessToken);
+
+  const { title, description } = req.body;
+  const todo = new Todo({
+    title,
+    description,
+    author: id,
+  });
+  try {
+    const savedTodo = await todo.save();
+    res.status(201).json(savedTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 export const updateTodo = async (req, res) => {
-    try {
-        const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!todo) {
-            return res.status(404).json({ message: "Todo not found" });
-        }
-        res.status(200).json(todo);
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
     }
-    catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 export const deleteTodo = async (req, res) => {
-    try {
-        const todo = await Todo.findByIdAndDelete(req.params.id);
-        if (!todo) {
-            return res.status(404).json({ message: "Todo not found" });
-        }
-        res.status(200).json({ message: "Todo deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
     }
-}
+    res.status(200).json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
